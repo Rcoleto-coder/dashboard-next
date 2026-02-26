39414a6032108e2cf49595297e309fd983334d0a
Rcoleto-coder <coleto.rafael@gmail.com>
2026-02-25 22:25:53 -0500

    feat: create AuthLayout component and refactor page structure

--- dashboard/app/(auth)/page.tsx
+++ dashboard/app/(auth)/page.tsx
@@ -6,50 +6,27 @@ import type { Metadata } from "next"
 import { HTML_TITLE_PREFIX } from "@/constants"
 
 // Components
-import { AppSidebar } from "@/components/app-sidebar"
 import { ChartAreaInteractive } from "@/components/chart-area-interactive"
 import { DataTable } from "@/components/data-table"
 import { SectionCards } from "@/components/section-cards"
-import { SiteHeader } from "@/components/site-header"
-import {
-  SidebarInset,
-  SidebarProvider,
-} from "@/components/ui/sidebar"
 
 // Data
 import data from "./dashboard/data.json"
 
 
-// Read the html title from the layout and add | Panel
+// Read the html title from the layout
 export const metadata : Metadata = {
   title: `${HTML_TITLE_PREFIX}`,
 }
 
 export default function Page() {
   return (
-    <SidebarProvider
-      style={
-        {
-          "--sidebar-width": "calc(var(--spacing) * 72)",
-          "--header-height": "calc(var(--spacing) * 12)",
-        } as React.CSSProperties
-      }
-    >
-      <AppSidebar variant="inset" />
-      <SidebarInset id="main">
-        <SiteHeader />
-        <div className="flex flex-1 flex-col">
-          <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              <SectionCards />
-              <div className="px-4 lg:px-6">
-                <ChartAreaInteractive />
-              </div>
-              <DataTable data={data} />
-            </div>
-          </div>
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
+    <>
+      <SectionCards />
+      <div className="px-4 lg:px-6">
+        <ChartAreaInteractive />
+      </div>
+      <DataTable data={data} />
+    </>
   )
 }
--- dashboard/app/(auth)/settings/page.tsx
+++ dashboard/app/(auth)/settings/page.tsx
@@ -1,6 +1,15 @@
 // Next
 import type { Metadata } from "next";
 
+//Components
+import {
+    Card,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+
 // Constants
 import { HTML_TITLE_PREFIX } from "@/constants"
 
@@ -8,11 +17,28 @@ export const metadata: Metadata = {
   title: `${HTML_TITLE_PREFIX} | Settings`,
 }
 
-export default function SettingsPage() {
-    return (
-        <div>
-            <h1>Settings</h1>
-        </div>
-    )
+// Todo: implement the getUser query
+// add mores fields to the user data - User name, phone number, profile picture?
 
+const user = {}
+
+export default function SettingsPage() {
+  return (
+    <div className="px-4 lg:px-6 space-y-4">
+      <h1>Settings</h1>
+      <Card className="@container/card">
+        <CardHeader>
+          <CardDescription>Card Header</CardDescription>
+          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+            $1,250.00
+          </CardTitle>
+        </CardHeader>
+        <CardFooter className="flex-col items-start gap-1.5 text-sm">
+          <div className="text-muted-foreground">
+            Card footer
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  )
 }
